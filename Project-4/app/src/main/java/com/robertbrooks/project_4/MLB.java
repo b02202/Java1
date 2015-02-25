5d5033e22176433255bf28f028f9dd4822bff57f
b02202 <rwbrooks99@fullsail.edu>
2015-02-25 17:24:41 -0500

    Added MLB custom data class

---
+++ Project-4/app/src/main/java/com/robertbrooks/project_4/MLB.java
@@ -0,0 +1,37 @@
+package com.robertbrooks.project_4;
+
+/**
+ * Created by Bob on 2/25/2015.
+ */
+public class MLB {
+
+    private String title;
+    private String author;
+    private String domain;
+
+    // Getters and Setters
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
+}
